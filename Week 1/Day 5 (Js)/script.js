b4b89a31168123435b1f6931cfa266d77b4a7cda
joelgeller <joelgeller21@gmail.com>
2023-01-29 10:11:54 +0200

    my second commit

---
+++ Week 1/Day 5 (Js)/script.js
@@ -0,0 +1,2 @@
+
+console.log("Hello how are you");
---
+++ Week 1/Day 5 (Js1)/script.js
@@ -0,0 +1,24 @@
+let favoriteFood = "Schnitzel";
+let favoriteMeal = "Breakfast";
+console.log("I eat " + favoriteFood + " for " + favoriteMeal );
+
+// Exercise 2 pt 1
+const myWatchedSeries = {"black mirror, money heist, the big bang theory"};
+let myWatchedSeriesLength = myWatchedSeries.length;
+
+let myWatchedSeriesSenence = myWatchedSeries[0] +", " + myWatchedSeries[1] + 
+
+const 
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b) //first expression
+// Prediction:
+// Actual:
+
+a = 2;
+
+console.log(a+b) //second expression
+// Prediction:
+// Actual:
